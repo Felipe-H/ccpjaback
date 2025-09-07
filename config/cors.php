c2f206832cee9e49124f0969620ea383ae0c55c9
Felipe-H <felipe.hborba@hotmail.com>
2025-09-07 20:37:11 -0300

    Ajuste no cors

--- config/cors.php
+++ config/cors.php
@@ -2,8 +2,11 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
-    'allowed_headers' => ['Content-Type','X-Requested-With','X-XSRF-TOKEN','Accept','Authorization'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000, https://ccpjaback.onrender.com'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'X-XSRF-TOKEN', 'Accept', 'Authorization'],
+    'allowed_origins_patterns' => [
+        '^https://.*\.vercel\.app$',
+    ],
     'exposed_headers' => ['Set-Cookie'],
     'max_age' => 0,
     'supports_credentials' => true,
