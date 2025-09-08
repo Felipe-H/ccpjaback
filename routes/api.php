f307a9b895d1bc1e2063b61ec70a20a5ed1cd560
Felipe-H <felipe.hborba@hotmail.com>
2025-09-07 22:31:43 -0300

    teste44

--- routes/api.php
+++ routes/api.php
@@ -7,29 +7,27 @@ use App\Http\Controllers\Api\InventoryItemController;
 use App\Http\Controllers\Api\FinancialRecordController;
 use App\Http\Controllers\Auth\AuthController;
 
-Route::options('{any}', fn () => response()->noContent())->where('any','.*');
+Route::group(['prefix' => 'api'], function () {
 
+    Route::options('{any}', fn () => response()->noContent())->where('any','.*');
 
-Route::post('/login',    [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::get('/ping', fn() => 'pong');
+    Route::post('/login',    [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/ping', fn() => 'pong');
 
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/user', fn (Request $r) => $r->user());
+        Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/user', fn (Request $r) => $r->user());
+        Route::get   ('/inventory-items',                 [InventoryItemController::class, 'index']);
+        Route::post  ('/inventory-items',                 [InventoryItemController::class, 'store']);
+        Route::put   ('/inventory-items/{item}',          [InventoryItemController::class, 'update']);
+        Route::delete('/inventory-items/{item}',          [InventoryItemController::class, 'destroy']);
+        Route::patch ('/inventory-items/{item}/quantity', [InventoryItemController::class, 'changeQuantity']);
 
-
-    Route::get   ('/inventory-items',                 [InventoryItemController::class, 'index']);
-    Route::post  ('/inventory-items',                 [InventoryItemController::class, 'store']);
-    Route::put   ('/inventory-items/{item}',          [InventoryItemController::class, 'update']);
-    Route::delete('/inventory-items/{item}',          [InventoryItemController::class, 'destroy']);
-    Route::patch ('/inventory-items/{item}/quantity', [InventoryItemController::class, 'changeQuantity']);
-
-
-    Route::get   ('/finance-records',           [FinancialRecordController::class, 'index']);
-    Route::post  ('/finance-records',           [FinancialRecordController::class, 'store']);
-    Route::put   ('/finance-records/{finance}', [FinancialRecordController::class, 'update']);
-    Route::delete('/finance-records/{finance}', [FinancialRecordController::class, 'destroy']);
-
-    Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get   ('/finance-records',           [FinancialRecordController::class, 'index']);
+        Route::post  ('/finance-records',           [FinancialRecordController::class, 'store']);
+        Route::put   ('/finance-records/{finance}', [FinancialRecordController::class, 'update']);
+        Route::delete('/finance-records/{finance}', [FinancialRecordController::class, 'destroy']);
+    });
 });
